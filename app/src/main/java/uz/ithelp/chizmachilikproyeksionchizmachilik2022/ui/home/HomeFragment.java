7984f03e04b33378f4201a1b755ea343cd1876ac
shoxumarzoda <ujBP3wXYUstD23d>
2022-09-10 09:59:39 +0500

    dashboardFragment

--- app/src/main/java/uz/ithelp/chizmachilikproyeksionchizmachilik2022/ui/home/HomeFragment.java
+++ app/src/main/java/uz/ithelp/chizmachilikproyeksionchizmachilik2022/ui/home/HomeFragment.java
@@ -52,14 +52,20 @@ public class HomeFragment extends Fragment implements UserAdapter.SelectedUser {
     private void initial() {
         modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"KIRISH                    ","asd"));
         modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"Chizmachilik fanining O‘zbekistonda rivojlanishi va o‘qitilishining qisqacha tarixi","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.1-§.\tBuyumni proyeksiyalar tekisliklariga proyeksiyalash","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.2-§.\tAsosiy ko‘rinishlar.","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"salom","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"salom","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"salom","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"salom","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"salom","asd"));
-        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"salom","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.1-§.\tBuyumni proyeksiyalar tekisliklariga proyeksiyalash                        ","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.2-§.\tAsosiy ko‘rinishlar.                                                       ","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.3-§.\tKesimlar va qirqimlar                  ","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.4-§.\tDetal eskizi va texnik rasmini tuzish","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.5-§.\tDetal chizmasida og‘ma qirqim bajarish","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.6-§.\tDetal chizmasida tekis qirqim bajarish","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.7-§.\tChizmachilik fani (ko’rinishlar, qirqim va kesimlar)dan grafik ish bajarish uchun topshiriqlar","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.8-§.\tDetal chizmasidagi mahalliy va qo‘shimcha ko‘rinishlar bajarilsin","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.9-§.\tDetalning berilgan ikki ko’rinishi asosida yetishmovchi uchinchi ko’rinishi topilsin, aksonometrik proyeksiyasi qurilsin. Ortogonal va aksonometrik proyeksiyasida qirqim tadbiq qilinsin","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.10-§.\tDetal chizmasida murakkab qirqim tadbiq qilinsin","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.11-§.\tDetalning yaqqol tasviri asosida uning eskiz va texnik rasmi bajarilsin","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.12-§.\tChizmachilik fani (ko’rinishlar, qirqim va kesimlar)dan test savollari","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.13-§.\tChizmachilik fani (ko’rinishlar, qirqim va kesimlar)dan  terminologik lug‘at","asd"));
+        modelRcyclers.add(new ModelRcycler(R.drawable.ic_home_black_24dp,"1.13-§.\tChizmachilik fani (ko’rinishlar, qirqim va kesimlar)dan  terminologik lug‘at","asd"));
 
     }
 
@@ -71,6 +77,7 @@ public class HomeFragment extends Fragment implements UserAdapter.SelectedUser {
 
     @Override
     public void selectedUser(ModelRcycler modelRcycler) {
+
 
     }
 }
